404ca2084c1ade504691870847128a86990eb1df
james <james@rubify.com>
2017-08-08 18:16:35 +0800

    change some style on mobile

--- gulpfile.js
+++ gulpfile.js
@@ -12,7 +12,7 @@ gulp.task('build', shell.task(['jekyll serve']));
 gulp.task('serve', function() {
   browserSync.init({ port: 8080, server: { baseDir: '_site/' } });
   // Reloads page when some of the already built files changed:
-  gulp.watch('_site/**/*.*').on('change', browserSync.reload);
+  // gulp.watch('_site/**/*.*').on('change', browserSync.reload);
 });
 
 gulp.task('default', ['build', 'serve']);
